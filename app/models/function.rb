ef876211ffa1697f4616ae6887b322e71e0f51de
Stuart Updegrave <stuart@updegrave.com>
2014-01-08 09:35:32 +0100

    add roles to seed data / bugfix nil user on org chart /  rename role_by_name to user_by_role

--- app/models/function.rb
+++ app/models/function.rb
@@ -4,25 +4,20 @@ class Function < ActiveRecord::Base
   
   validates :name, uniqueness: { case_sensitive: false }
   
-  # def leads 
-  #   leads_roles = self.roles.reject { |role| !["Lead", "Co-Lead", "Mentor"].include? role.name }
-  #   leads_roles.map { |role| role.users.first }
-  # end
-  
   def lead
-    role_by_name "Lead"
+    user_by_role "Lead"
   end
 
   def colead
-    role_by_name "Co-Lead"
+    user_by_role "Co-Lead"
   end
 
   def mentor
-    role_by_name "Mentor"
+    user_by_role "Mentor"
   end
 
   private 
-    def role_by_name(name)
+    def user_by_role(name)
       User.with_role(name, self).first
     end
 
@@ -30,7 +25,4 @@ class Function < ActiveRecord::Base
       name_for_email = self.name.parameterize('_')
       self.update_attribute :email, "#{name_for_email}@goingnowhere.org" 
     end
-  
-  # can't enforce uniqueness unless also validating presence 
-  # validates :email, uniqueness: { case_sensitive: false }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name(role)
   puts 'role: ' << role
 end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.confirm!
-user.add_role :admin
+
+# puts 'DEFAULT USERS'
+# user = User.find_or_create_by_email :first_name => ENV['ADMIN_FIRST_NAME'].dup, 
+#                                     :last_name => ENV['ADMIN_LAST_NAME'].dup, 
+#                                     :email => ENV['ADMIN_EMAIL'].dup, 
+#                                     :password => ENV['ADMIN_PASSWORD'].dup, 
+#                                     :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+# puts 'user: ' << user.name
+# user.confirm!
+# user.add_role :admin
